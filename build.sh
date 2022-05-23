8c867e1e0bb6e3a66b5724d71f8186f60470c127
Anton Tsaregorodtsev <TsaregorodtsevAnton@gmail.com>
2022-05-23 15:04:38 +0300

    Added build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+PAKET_TOOL_PATH=.paket
+CAKE_TOOL_PATH=.cake
+PAKET="$PAKET_TOOL_PATH"/paket
+CAKE = "$CAKE_TOOL_PATH"/cake
+
+if ! [ -e "$PAKET" ]
+then
+  dotnet tool install Paket --version 7.1.5 --tool-path "$PAKET_TOOL_PATH"
+fi
+
+if ! [ -e "$CAKE" ]
+then
+  dotnet tool install Cake.Tool --version 2.2.0 --tool-path "$CAKE_TOOL_PATH"
+fi
+
+"$PAKET" install
+dotnet restore
+export MSBUILDSINGLELOADCONTEXT=1
+export MSBUILDDISABLENODEREUSE=1
+
+"$CAKE" -- "$@"
